c0f1b20d736585d3c9ad958a3c404e12c269e468
Florian Strohmaier <florian.strohmaier@icinga.com>
2019-05-21 17:08:52 +0200

    Fix main layout for use without js
    fixes #3629

--- public/js/icinga/ui.js
+++ public/js/icinga/ui.js
@@ -636,9 +636,6 @@
                         this.mobileMenu = true;
                     }
                 } else {
-                    $headerLogo.css({
-                        top: $header.css('height')
-                    });
                     $main.css({
                         top: $header.css('height')
                     });
